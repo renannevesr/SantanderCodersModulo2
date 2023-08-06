1b067e3684e5d6c1de93d5a5e9f6fb3b806ee3ab
Bowie <breendamonteiro08@gmail.com>
2023-08-06 18:14:06 -0300

    adicionar e buscar cliente_cpf
    Co-authored-by: Renan Rego <renannevesr@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+clientes = []
+class Clientes:
+    def __init__(self, cpf, nome, data_nascimento):
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+
+
+def adicionar_cliente():
+    cpf = input("Digite o CPF do cliente: ")
+    nome = input("Digite o Nome do cliente: ")
+    data_nascimento = input("Digite a Data de Nascimento do cliente: ")
+
+    novo_cliente = Clientes(cpf, nome, data_nascimento)
+    clientes.append(novo_cliente)
+
+def busca_cliente_cpf():
+    cpf = input("Digite o CPF do cliente: ")
+    for cliente in clientes:
+        if cliente.cpf == cpf:
+            print(cliente.nome)
+            return cliente
+class MedicamentosFitoterapicos():
+    def __init__(self, nome, composto_principal, laboratorio, descricao):
+        self.nome = nome
+        self.composto_principal = composto_principal
+        self.laboratorio = laboratorio
+        self.descricao = descricao
+
+# class MedicamentosQuimioterapicos(MedicamentosFitoterapicos):
+#     def __init__(self, nome, composto_principal, laboratorio, descricao):
+#         MedicamentosFitoterapicos.__init__(self, nome = nome, composto_principal = composto_principal, laboratorio = laboratorio, descricao = descricao, necessita_receita)
+     #   self.necessita_receita = necessita_receita
+
+
+
+
 def main():
     while True:
         menu_str = """
@@ -11,24 +48,16 @@ def main():
 
         opcao = input("Escolha uma opção: ")
         if opcao == '1':
-            print("opção 1")
+            adicionar_cliente()
         elif opcao == '2':
-            print("opção 2")
+            busca_cliente_cpf()  
+        elif opcao == '3':
+            print("Clientes\/")
+            print(clientes[0].cpf, clientes[0].nome, clientes[0].data_nascimento)
+
         elif opcao == '3':
             print("Saindo...")
             break
-
-class Clientes:
-    def __init__(self, cpf, nome, data_nascimento):
-        self.cpf = cpf
-        self.nome = nome
-        self.data_nascimento = data_nascimento
-    
-
-class MedicamentosFitoterapicos():
-    def __init__():
-        pass
-
 
 if __name__ == "__main__":
     main()
